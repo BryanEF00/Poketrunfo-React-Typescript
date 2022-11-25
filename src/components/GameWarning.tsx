7f4729048a8e0ae6349c35c87be89dec8746cc43
BryanEF00 <bryan.efernandes@gmail.com>
2022-11-25 14:50:05 -0300

    fix: responsive leave game message

--- src/components/GameWarning.tsx
+++ src/components/GameWarning.tsx
@@ -9,9 +9,17 @@ function GameWarning({ setShowWarning }: IProps) {
   const navigate = useNavigate();
 
   return (
-    <div className="w-full h-screen fixed top-0 left-0 flex items-center justify-center">
+    <div className="w-full h-screen fixed top-0 left-0 flex items-center justify-center z-[60]">
       <div className="w-full h-full flex justify-center items-center backdrop-blur-sm backdrop-brightness-50">
-        <div className="w-4/5 bg-white dark:bg-neutral-800 grid grid-cols-2 place-items-center gap-2 p-5 rounded-lg">
+        <div
+          className="w-4/5 bg-white dark:bg-neutral-800 grid
+            grid-cols-2 place-items-center gap-2 p-5 rounded-lg
+            md:w-2/5
+            lg:w-1/3
+            2xl:w-1/5
+            3xl:w-1/6
+          "
+        >
           <div className="col-span-2 mb-5">Leave the game?</div>
           <button
             className="w-full col-span-1 text-white bg-red-500 py-2 rounded-lg"
