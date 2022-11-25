8942422077b32a86be3fde7627eb015e18f78711
BryanEF00 <bryan.efernandes@gmail.com>
2022-11-25 14:11:43 -0300

    fix: force selectedPokemons to load

--- src/components/GenerateTeam.tsx
+++ src/components/GenerateTeam.tsx
@@ -1,8 +1,11 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import AppContext from '../context/AppContext';
 import randomPokemon from '../helpers/collection/randomPokemon';
 import randomTeam from '../helpers/collection/randomTeam';
-import { saveCollection } from '../services/localStorage/collection';
+import {
+  readCollection,
+  saveCollection,
+} from '../services/localStorage/collection';
 
 function GenerateTeam() {
   const { state, setState, pokemons, selectedPokemons } =
@@ -21,6 +24,10 @@ function GenerateTeam() {
     }
   };
 
+  useEffect(() => {
+    readCollection();
+  }, [selectedPokemons]);
+
   const handleGenerateTeam = () => {
     const selectedTeam = randomTeam(pokemons);
     setState({ ...state, selectedPokemons: selectedTeam });
@@ -38,44 +45,42 @@ function GenerateTeam() {
         <h1 className="w-1/2 font-bold uppercase ">Collection :</h1>
       </div>
       <h3 className="text-center">Add a random Pokémon or Team.</h3>
-      {selectedPokemons && (
-        <div className="w-full flex flex-wrap justify-center gap-3">
-          <button
-            type="button"
-            className="w-[45%] bg-green-500 text-white font-semibold py-1 rounded-lg disabled:opacity-50
+      <div className="w-full flex flex-wrap justify-center gap-3">
+        <button
+          type="button"
+          className="w-[45%] bg-green-500 text-white font-semibold py-1 rounded-lg disabled:opacity-50
           md:w-[35%]
           lg:w-[20%]
           xl:w-[15%]"
-            disabled={selectedPokemons?.length === 6}
-            onClick={handleGeneratePokemon}
-          >
-            + Pokémon
-          </button>
-          <button
-            type="button"
-            className="w-[45%] bg-[#9E69E3] text-white font-semibold py-1 rounded-lg disabled:opacity-50
+          disabled={selectedPokemons?.length === 6}
+          onClick={handleGeneratePokemon}
+        >
+          + Pokémon
+        </button>
+        <button
+          type="button"
+          className="w-[45%] bg-[#9E69E3] text-white font-semibold py-1 rounded-lg disabled:opacity-50
           md:w-[35%]
           lg:w-[20%]
           xl:w-[15%]"
-            disabled={selectedPokemons?.length > 0}
-            onClick={handleGenerateTeam}
-          >
-            + Team
-          </button>
-          <button
-            type="button"
-            className="w-[45%] border-2 py-1 font-semibold border-red-600 rounded-lg text-red-600 bg-zinc-100 
+          disabled={selectedPokemons?.length > 0}
+          onClick={handleGenerateTeam}
+        >
+          + Team
+        </button>
+        <button
+          type="button"
+          className="w-[45%] border-2 py-1 font-semibold border-red-600 rounded-lg text-red-600 bg-zinc-100 
           dark:bg-neutral-900 disabled:opacity-50
           md:w-[35%]
           lg:w-[20%]
           xl:w-[15%]"
-            onClick={handleRemoveAll}
-            disabled={selectedPokemons?.length === 0}
-          >
-            Remove All
-          </button>
-        </div>
-      )}
+          onClick={handleRemoveAll}
+          disabled={selectedPokemons?.length === 0}
+        >
+          Remove All
+        </button>
+      </div>
     </div>
   );
 }
